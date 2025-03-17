165ce97afeac0f1c94b34722cbf6f4cd5132abd0
santosh kumbhkar <santosh.kumbhkar3014@gmail.com>
2025-03-17 14:36:30 +0530

    commit for checking login logic updated in controller

--- Server/Controller/authController.js
+++ Server/Controller/authController.js
@@ -7,29 +7,29 @@ import jwt from 'jsonwebtoken';
 
 //  for sign up 
 export const signup = async (req, res) => {
-    const { FirstName,LastName ,Email, Password } =req.body;
+    const { FirstName, LastName, Email, Password } = req.body;
 
     // it ensure unique user get stored 
 
-    const emailexist =await User.findone({Email});
-    if(emailexist){
-        return res.status(400).json({message:"use unique email"});
+    const emailexist = await User.findone({ Email });
+    if (emailexist) {
+        return res.status(400).json({ message: "use unique email" });
 
     }
     const hashedPass = await bcrypt.hash(Password, 10);
 
     try {
-       const signUser = new User({
-           FirstName,
-           LastName,
-           Email,
-           Password:hashedPass
-       });
-       await signUser.save();
-       res.status(201).json({message: "user resisttered successfully"});
+        const signUser = new User({
+            FirstName,
+            LastName,
+            Email,
+            Password: hashedPass
+        });
+        await signUser.save();
+        res.status(201).json({ message: "user resisttered successfully" });
 
     } catch (error) {
-         console.log("error while registering user", error);
+        console.log("error while registering user", error);
     }
 };
 
@@ -38,32 +38,36 @@ export const signup = async (req, res) => {
 // for login
 
 export const login = async (req, res) => {
-    const { Email, Password } =req.body;
+    try {
+    const { Email, Password } = req.body;
 
-    const logUser =await User.findone({Email});
-    const ismatch = await bcrypt.compare(Password ,logUser.Password);
-    if(!logUser|| !ismatch){
-        return res.status(400).json({message:"Invalid credinatials"});
+    const logUser = await User.findone({ Email });
+    const ismatch = await bcrypt.compare(Password, logUser.Password);
+    if (!logUser || !ismatch) {
+        return res.status(400).json({ message: "Invalid credinatials" });
 
     }
-  
     // token 
 
-    const token =jwt.sign({id:logUser._id},process.env.secret-key,{expiresIn:"1h"});
-    res.status(200).json({token});
+    const token = jwt.sign({ id: logUser._id }, process.env.secret - key, { expiresIn: "1h" });
+    console.log(`User logged in succefully ${Email}`);
+    res.status(200).json({ message :"login succesfull ," ,token });
+    } catch (error) {
+        res.status(400)
+    }
 }
 
-  
+
 
 //  user details
 
-export const getuser =async(req, res)=>{
-     try {
-        const userdet =await User.findByid(req.user.id).select("-Password");
-     res.json(userdet);
-     } catch (error) {
-        res.status(400).json({message:"user not found"});
-     }
+export const getuser = async (req, res) => {
+    try {
+        const userdet = await User.findByid(req.user.id).select("-Password");
+        res.json(userdet);
+    } catch (error) {
+        res.status(400).json({ message: "user not found" });
+    }
 }
 
 
